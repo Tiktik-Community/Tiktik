070571dba0fe87c83f06c3579f0c86976b4f24a6
Programming-School-Pro-Coding <mohamedehabam99@hotmail.com>
2022-07-22 14:13:06 +0200

    Added Follow Functionality

--- pages/api/profile/[id].ts
+++ pages/api/profile/[id].ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-import { singleUserQuery, userCreatedPostsQuery, userLikedPostsQuery } from './../../../utils/queries';
+import { singleUserQuery, userCreatedPostsQuery, userLikedPostsQuery, FollowersQuery } from './../../../utils/queries';
 import { client } from '../../../utils/client';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
@@ -10,13 +10,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const query = singleUserQuery(id);
     const userVideosQuery = userCreatedPostsQuery(id);
     const userLikedVideosQuery = userLikedPostsQuery(id);
-
+    const userFollowersQuery = FollowersQuery(id);
+    
     const user = await client.fetch(query);
     const userVideos = await client.fetch(userVideosQuery);
     const userLikedVideos = await client.fetch(userLikedVideosQuery);
-
-    const data = { user: user[0], userVideos, userLikedVideos };
-
+    const userFollowers = await client.fetch(userFollowersQuery);
+    
+    const data = { user: user[0], userVideos, userLikedVideos, userFollowers };
+    
     res.status(200).json(data);
+  } else if (req.method === "POST") {
+    const { id } = req.query;
+    const userFollowersQuery = FollowersQuery(id);
+
+    client.fetch(userFollowersQuery).then((response) => {
+      response.followers++
+    })
   }
 }
