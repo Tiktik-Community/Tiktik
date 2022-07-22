19c2f3527762d72c0b549fcdb5055e8d68406f9b
Programming-School-Pro-Coding <mohamedehabam99@hotmail.com>
2022-07-22 10:32:17 +0200

    Update About.tsx

--- pages/About.tsx
+++ pages/About.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Image from "next/image";
 
-import icon from "/favicon.png";
-
 const About = () => {
   return (
     <>
       <div className="flex flex-col gap-10 h-full">
         <div className="w-full">
-          <Image src={icon} alt="Icon" layout="responsive" />
+          <Image
+            src="https://i.imgur.com/XuODa9Q.png"
+            alt="Icon"
+            layout="responsive"
+          />
         </div>
         <h1 className="text-4xl font-semibold">Tiktik</h1>
         <p className="text-center">
